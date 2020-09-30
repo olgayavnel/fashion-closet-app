4af336f9ec20891068e3f1accd2a67959a0e4b5f
yavnelf <54780131+Olga-Yavnel@users.noreply.github.com>
2020-09-30 23:20:32 +0200

    third commit

--- src/edit.js
+++ src/edit.js
@@ -27,6 +27,22 @@ removeEl.addEventListener('click', (e) => {
     location.assign('/index.html')
 })
 
+//Uploading a file using jQuery
+
+$(function () {
+    $(":file").change(function () {
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+            reader.onload = imageIsLoaded;
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
+});
+
+function imageIsLoaded(e) {
+    $('#myImg').attr('src', e.target.result);
+};
+
 window.addEventListener('storage', (e) => {
     if (e.key === 'looks') {
         initializeEditPage(lookId)
